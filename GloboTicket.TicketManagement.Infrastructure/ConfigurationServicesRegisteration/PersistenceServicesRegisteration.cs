0bfb71bc9d15b0f8da8885b0b97e1f85d9225b4a
muhammad qzih <muhammadghqzih@gmail.com>
2025-08-31 13:10:32 +0300

    Imlement infrastructre repositories and add persistence configuration

---
+++ GloboTicket.TicketManagement.Infrastructure/ConfigurationServicesRegisteration/PersistenceServicesRegisteration.cs
@@ -0,0 +1,30 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Infrastructure.ConfigurationServicesRegisteration
+{
+    public static class PersistenceServicesRegisteration
+    {
+        public static IServiceProvider AddPersistenceServices(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<GloboTicketDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("GloboTicketConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<IOrderRepository, >();
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ GloboTicket.TicketManagement.Infrastructure/Repositories/EventRepository.cs
@@ -0,0 +1,24 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Infrastructure.Repositories
+{
+    public class EventRepository : BaseRepository<Event>, IEventRepository
+    {
+        public EventRepository(GloboTicketDbContext context) : base(context)
+        {
+        }
+        public Task<bool> IsEventNameAndDateUnique(string eventName, DateTime eventDate)
+        {
+            var matches = _context.Events
+                .Any(e => e.EventName.Equals(eventName) && e.Date.Equals(eventDate));
+
+            return Task.FromResult(matches);
+        }
+    }
+}
